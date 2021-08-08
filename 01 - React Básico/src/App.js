433a6fb74ad7bd300ac6bcd24ce00b6e325fccc1
matheusz98 <matheuscastiglia31@gmail.com>
2021-08-08 12:24:59 -0300

    Estrutura inicial do projeto

---
+++ 01 - React Básico/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+    state = {
+      name: 'Matheus Costa',
+      counter: 0
+    };
+
+  handlePClick = () => {
+    this.setState({ name: 'Matheuzito' });
+  }
+
+  handleAClick = (e) => {
+    e.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p onClick={this.handlePCLick}>
+              {name} {counter}
+          </p>
+          <a
+          onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+export default App;
