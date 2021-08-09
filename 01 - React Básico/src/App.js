ece2160bdc2000e3c81f9c3589373b57aa143dcd
matheusz98 <matheuscastiglia31@gmail.com>
2021-08-09 17:32:20 -0300

    Data fetching

--- 01 - React Básico/src/App.js
+++ 01 - React Básico/src/App.js
@@ -1,49 +1,50 @@
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-    state = {
-      posts: []
-    };
+  state = {
+    posts: [],
+  };
 
-    componentDidMount() {
-      this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'O título 1',
-            body: 'O corpo 1'
-          },
-  
-          {
-            id: 2,
-            title: 'O título 2',
-            body: 'O corpo 2'
-          },
-  
-          {
-            id: 3,
-            title: 'O título 3',
-            body: 'O corpo 3'
-          },
-        ]
-      });
-    }
+  componentDidMount() {
+    this.loadPosts();
+  }
 
-    render() {
-      const { posts } = this.state;
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-      return (
-        <div className="App">
-          {posts.map(post => (
-            <div key={post.id}>
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
             </div>
           ))}
         </div>
-      );
-    }
+      </section>
+    );
+  }
 }
 
 export default App;
