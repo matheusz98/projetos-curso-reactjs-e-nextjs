9d5ba8520f4ceb4e315ffa918519145125ea0ee6
matheusz98 <matheuscastiglia31@gmail.com>
2021-08-09 15:08:00 -0300

    Lifecycle

--- 01 - React Básico/src/App.js
+++ 01 - React Básico/src/App.js
@@ -3,41 +3,47 @@ import { Component } from 'react';
 
 class App extends Component {
     state = {
-      posts: [
-        {
-          id: 1,
-          title: 'O título 1',
-          body: 'O corpo 1'
-        },
-
-        {
-          id: 2,
-          title: 'O título 2',
-          body: 'O corpo 2'
-        },
-
-        {
-          id: 3,
-          title: 'O título 3',
-          body: 'O corpo 3'
-        },
-      ]
+      posts: []
     };
 
-  render() {
-    const { posts } = this.state;
+    componentDidMount() {
+      this.setState({
+        posts: [
+          {
+            id: 1,
+            title: 'O título 1',
+            body: 'O corpo 1'
+          },
+  
+          {
+            id: 2,
+            title: 'O título 2',
+            body: 'O corpo 2'
+          },
+  
+          {
+            id: 3,
+            title: 'O título 3',
+            body: 'O corpo 3'
+          },
+        ]
+      });
+    }
 
-    return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+      const { posts } = this.state;
+
+      return (
+        <div className="App">
+          {posts.map(post => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
 }
 
 export default App;
