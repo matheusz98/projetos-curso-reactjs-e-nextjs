909e6b87345ff016bc973de758d569f9387162ae
Matheus Costa <matheuscastiglia31@gmail.com>
2021-08-11 22:36:47 -0300

    Trabalhando com input de texto

---
+++ 01 - React Básico/src/components/TextInput.jsx
@@ -0,0 +1,14 @@
+import './TextInput.css';
+
+export const TextInput = ({ searchValue, handleChange }) => {
+    return (
+        <input
+          className="text-input"
+          onChange={handleChange}
+          value={searchValue}
+          type="search"
+          placeholder="Type your search"
+        >
+        </input>
+    );
+}
