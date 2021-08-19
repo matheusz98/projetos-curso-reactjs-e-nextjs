cd459addb8e970491b17fffe7efce2918038099f
Matheus Costa <matheuscastiglia31@gmail.com>
2021-08-19 17:24:20 -0300

    Configurações iniciais

---
+++ 03 - Projeto Landing Page - Strapi e MongoDB/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c63f845c4e942b495dbb4e773faddbab'),
+    },
+  },
+});
---
+++ 03 - Projeto Landing Page - Strapi e MongoDB/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a07bec95-1c72-4edf-adda-119435e06dba'
+};
